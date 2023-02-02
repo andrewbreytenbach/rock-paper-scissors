21be4cc807522516c59841958cdb5432de91faad
Andrew Breytenbach <ambreyts15@gmail.com>
2023-02-02 08:43:46 -0500

    added else if statements to the code

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 function getComputerChoice() {
    const choices = ['Rock', 'Paper', 'Scissors'];
     const randomIndex = Math.floor(Math.random() * choices.length);
-    return choices[randomIndex]
+    return choices[randomIndex];
 }
 
-function playRound(playerSelection, computerSelection) {
-    // your code here!
-  }
+function playRound(playerSelection, computerSelection) 
+if (playerSelection === computerSelection) {
+    return `It's a tie! you both picked ${playerSelection}`;
+ } else if (playerSelection === "rock" && computerSelection === "scissors") {
+    return "You win! Rock beats Scissors";
+ } else if (playerSelection === "paper" && computerSelection === "rock") {
+    return "You win! Paper beats Rock";
+ } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    return "You win! Scissors beats Paper";
+ } else {
+    return `You lose! ${computerSelection} beats ${playerSelection}`;
+ }
+
    
   const playerSelection = "rock";
   const computerSelection = getComputerChoice();
