959b325dccf65c39f9984063690f863d774f7e52
Andrew Breytenbach <ambreyts15@gmail.com>
2023-02-02 08:34:32 -0500

    added more functions to the js code

--- script.js
+++ script.js
@@ -4,4 +4,12 @@ function getComputerChoice() {
     return choices[randomIndex]
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
+    // your code here!
+  }
+   
+  const playerSelection = "rock";
+  const computerSelection = getComputerChoice();
+  console.log(playRound(playerSelection, computerSelection));
+
+// console.log(getComputerChoice());
