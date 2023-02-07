ea9e9041edf359495a366fb4e84ceeda1dd9be68
Andrew Breytenbach <117181767+andrewbreytenbach@users.noreply.github.com>
2023-02-07 17:55:05 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,45 +1,26 @@
-function getComputerChoice() {
-   const choices = ['Rock', 'Paper', 'Scissors'];
-    const randomIndex = Math.floor(Math.random() * choices.length);
-    return choices[randomIndex];
-}
 
-function playRound(playerSelection, computerSelection) 
-if (playerSelection === computerSelection) {
-    return `It's a tie! you both picked ${playerSelection}`;
- } else if (playerSelection === "rock" && computerSelection === "scissors") {
-    return "You win! Rock beats Scissors";
- } else if (playerSelection === "paper" && computerSelection === "rock") {
-    return "You win! Paper beats Rock";
- } else if (playerSelection === "scissors" && computerSelection === "paper") {
-    return "You win! Scissors beats Paper";
- } else {
-    return `You lose! ${computerSelection} beats ${playerSelection}`;
- }
-
-   
-  const playerSelection = "rock";
-  const computerSelection = getComputerChoice();
-  console.log(playRound(playerSelection, computerSelection));
-
-// console.log(getComputerChoice());
+var playerSelection = prompt("Please choose R, P, or S");
 
 function getComputerChoice() {
   const choices = ['R', 'P', 'S'];
   const randomIndex = Math.floor(Math.random() * choices.length);
   return choices[randomIndex];
 }
+
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
+
 function playRound(playerSelection, computerSelection) {
-  if (playerSelection === computerSelection) {
+  if (playerSelection === getComputerChoice()) {
     return `It's a tie! you both picked ${playerSelection}`;
-  } else if (playerSelection === "R" && computerSelection === "S") {
+  } else if (playerSelection === "R" && getComputerChoice() === "S") {
     return "You win! Rock beats Scissors";
-  } else if (playerSelection === "P" && computerSelection === "R") {
+  } else if (playerSelection === "P" && getComputerChoice() === "R") {
     return "You win! Paper beats Rock";
-  } else if (playerSelection === "S" && computerSelection === "P") {
+  } else if (playerSelection === "S" && getComputerChoice() === "P") {
     return "You win! Scissors beats Paper";
   } else {
-    return `You lose! ${computerSelection} beats ${playerSelection}`;
+    return `You lose! ${getComputerChoice()} beats ${playerSelection}`;
   }
 }
 
@@ -47,15 +28,14 @@ function roundResults(playerSelection, computerSelection) {
   alert(playRound(playerSelection, computerSelection));
 }
 
-
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
-
 function roundResults(playerSelection, computerSelection) {
   const result = playRound(playerSelection, computerSelection);
   alert(result);
 }
 
 roundResults(playerSelection, computerSelection);
+
+
+
 
 
